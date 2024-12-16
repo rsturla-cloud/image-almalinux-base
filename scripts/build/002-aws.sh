670d70d92cd6d64f523e63e175a5a243c5a10ed0
Robert Sturla <robertsturla@outlook.com>
2024-12-16 23:16:06 +0000

    chore: enable full build

--- scripts/build/002-aws.sh
+++ scripts/build/002-aws.sh
@@ -1,40 +1,40 @@
-# #!/usr/bin/env bash
+#!/usr/bin/env bash
 
-# set -euo pipefail
+set -euo pipefail
 
-# echo "Installing AWS CLI..."
-# ARCH=$(rpm -E %{_arch})
+echo "Installing AWS CLI..."
+ARCH=$(rpm -E %{_arch})
 
-# curl -Lo /tmp/awscli https://awscli.amazonaws.com/awscli-exe-linux-${ARCH}.zip
-# unzip -q /tmp/awscli -d /tmp/
+curl -Lo /tmp/awscli https://awscli.amazonaws.com/awscli-exe-linux-${ARCH}.zip
+unzip -q /tmp/awscli -d /tmp/
 
-# /tmp/aws/install --bin-dir /usr/bin --install-dir /usr/lib/aws-cli
-
-
-# echo "Installing ECR Credential Helper..."
-# ARCH=$(rpm -E %{_arch})
-# case "$ARCH" in
-#   aarch64) ARCH="arm64" ;;
-#   x86_64) ARCH="amd64" ;;
-# esac
-
-# LATEST_TAG=$(curl -LsSf https://api.github.com/repos/awslabs/amazon-ecr-credential-helper/releases/latest | jq -r ".tag_name" | cut -c 2-)
-
-# curl -LsSf -o /usr/bin/docker-credential-ecr-login https://amazon-ecr-credential-helper-releases.s3.us-east-2.amazonaws.com/${LATEST_TAG}/linux-${ARCH}/docker-credential-ecr-login
-# chmod +x /usr/bin/docker-credential-ecr-login
+/tmp/aws/install --bin-dir /usr/bin --install-dir /usr/lib/aws-cli
 
 
-# echo "Installing EC2 Services and Helpers"
-# ARCH=$(rpm -E %{_arch})
-# case "$ARCH" in
-#   aarch64) ARCH="arm64" ;;
-#   x86_64) ARCH="amd64" ;;
-# esac
-# dnf install -y \
-#   https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/linux_${ARCH}/amazon-ssm-agent.rpm \
-#   ec2-instance-connect \
-#   amazon-ec2-utils \
-#   cloud-init
+echo "Installing ECR Credential Helper..."
+ARCH=$(rpm -E %{_arch})
+case "$ARCH" in
+  aarch64) ARCH="arm64" ;;
+  x86_64) ARCH="amd64" ;;
+esac
 
-# # Enable services to start on boot
-# systemctl enable amazon-ssm-agent
+LATEST_TAG=$(curl -LsSf https://api.github.com/repos/awslabs/amazon-ecr-credential-helper/releases/latest | jq -r ".tag_name" | cut -c 2-)
+
+curl -LsSf -o /usr/bin/docker-credential-ecr-login https://amazon-ecr-credential-helper-releases.s3.us-east-2.amazonaws.com/${LATEST_TAG}/linux-${ARCH}/docker-credential-ecr-login
+chmod +x /usr/bin/docker-credential-ecr-login
+
+
+echo "Installing EC2 Services and Helpers"
+ARCH=$(rpm -E %{_arch})
+case "$ARCH" in
+  aarch64) ARCH="arm64" ;;
+  x86_64) ARCH="amd64" ;;
+esac
+dnf install -y \
+  https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/linux_${ARCH}/amazon-ssm-agent.rpm \
+  ec2-instance-connect \
+  amazon-ec2-utils \
+  cloud-init
+
+# Enable services to start on boot
+systemctl enable amazon-ssm-agent
