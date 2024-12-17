8827258e10d2a6b508450414c844de814a74fe83
Robert Sturla <robertsturla@outlook.com>
2024-12-17 23:25:48 +0000

    fix: disable openscap remediations (#8)

--- scripts/build/999-security.sh
+++ scripts/build/999-security.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 
-set -euo pipefail
+set -euox pipefail
 
 update-crypto-policies --no-reload --set FIPS
 
 dnf install -y authselect
 authselect select minimal --force
 
-dnf copr enable -y packit/OpenSCAP-openscap-maint-1.3 centos-stream-${OS_VERSION}-x86_64
-dnf install -y openscap-utils scap-security-guide openscap-engine-sce
-mv /usr/bin/oscap-bootc.new /usr/bin/oscap-bootc
+# dnf copr enable -y packit/OpenSCAP-openscap-maint-1.3 centos-stream-${OS_VERSION}-x86_64
+# dnf install -y openscap-utils scap-security-guide openscap-engine-sce
+# mv /usr/bin/oscap-bootc.new /usr/bin/oscap-bootc
 
-oscap-bootc --profile xccdf_org.ssgproject.content_profile_cis --fetch-remote-resources /usr/share/xml/scap/ssg/content/ssg-almalinux${OS_VERSION}-ds.xml
+# oscap-bootc --profile xccdf_org.ssgproject.content_profile_cis --fetch-remote-resources /usr/share/xml/scap/ssg/content/ssg-almalinux${OS_VERSION}-ds.xml
 
-dnf remove -y openscap-utils scap-security-guide openscap-engine-sce
-dnf copr remove -y packit/OpenSCAP-openscap-maint-1.3
+# dnf remove -y openscap-utils scap-security-guide openscap-engine-sce
+# dnf copr remove -y packit/OpenSCAP-openscap-maint-1.3
